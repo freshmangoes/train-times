d4f57851ed985524e26a8a7c9533ac3a4bfab9a6
Kyle C <kylecilia@gmail.com>
2019-11-11 22:36:41 -0800

    Updates arrival times and minutes away

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ firebase.analytics();
 // variable for firebase database
 var db = firebase.database();
 
+
 // jQuery caching to make my life easier :)
 var trainName = $('#name-input');
 var destination = $('#destination-input');
@@ -23,12 +24,15 @@ var firstTime = $('#firsttime-input');
 var frequency = $('#frequency-input');
 var trainTable = $('#train-table');
 
-$(document).ready(()=> {
+var trainCounter = 0;
 
+$(document).ready(()=> {
+  updateTimes();
 });
 
 $('#add-train').click((event)=> {
   event.preventDefault();
+
   var trainInput, destinationInput, firstTimeInput, frequencyInput;
 
   trainInput = trainName.val().trim();
@@ -64,21 +68,17 @@ $('#add-train').click((event)=> {
       minsAway
     });
 
+
     // user input invalid
   } else if(!moment(firstTimeInput, "HH:mm").isValid()){
-    
-    // debug
     console.error("not a valid time input");
-    
-    // lame attempt to show modal, not working lol
-    // $('#not-valid-modal').modal('show');
   }
 });
 
 // on child added to db
 db.ref().on("child_added", (childSnap) => {
-  var snapVal = childSnap.val();
-  console.log(`Child snapVal ${snapVal}`);
+  // var snapVal = childSnap.val();
+  // console.log('childSnap val:', snapVal);
   // populate the table with information from the child added
   populateTable(childSnap);
 });
@@ -115,12 +115,60 @@ var calculateNextArrival = (first, interval) => {
     // debug
     console.log(`Next time iteration: ${tempTime}`);
   }
+
   // return the tempTime to get next train arrival
   // return tempTime.format("HH:mm");
   return tempTime;
 };
 
-var minutesAway = (current, next) => {
-  return next.diff(current, "minutes");
+var minutesAway = (now, then) => {
+  // console.log("NOW:", now);
+  // console.log("THEN:", then);
+  return then.diff(now, "minutes");
 };
 
+var updateTimes = () => {
+  db.ref().once('value', (snap) => {
+    snap.forEach((childSnap) => {
+      var upDest = childSnap.val().destinationInput;
+      var upName = childSnap.val().trainInput;
+      var upFreq = childSnap.val().frequencyInput;
+
+      // updating nextTrainArrival
+      var tempFirst = moment(moment(childSnap.val().nextTrainArrival, "hh:mm A").format("HH:mm"), "HH:mm");
+      var tempInterval = childSnap.val().frequencyInput;
+
+      // console.log('tempFirst', tempFirst)
+      var upNext = calculateNextArrival(tempFirst, tempInterval);
+      // console.log('upNext', upNext);
+      
+      
+      // updating minutesAway
+      var tempNow = moment();
+      // console.log('tempThen', tempThen)
+      var upMinsAway = minutesAway(tempNow, upNext);
+      
+      // console.log('upMinsAway', upMinsAway);
+  
+      // format upNext into something usable
+      upNext = upNext.format("hh:mm A");
+      
+      // gets the key for the child
+      var key = childSnap.key;
+      var updateData = {
+        destinationInput: upDest,
+        trainInput: upName,
+        frequencyInput: upFreq,
+        nextTrainArrival: upNext,
+        minsAway: upMinsAway
+      };
+      // empty object for updates
+      var updates = {};
+      // turns key into the child directory/node nand adds update object
+      updates['/' + key] = updateData;
+      // console.log('updates:', updates);
+      // pushes updates to database
+      db.ref().update(updates);
+    });
+  });
+}
